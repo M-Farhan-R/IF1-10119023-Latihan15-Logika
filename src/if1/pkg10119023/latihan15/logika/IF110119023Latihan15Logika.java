c36d2f22bf07c755789e4e846e534eb07ace9f97
Farhan <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-20 20:42:07 +0700

    Latihan 15 - Logika

---
+++ src/if1/pkg10119023/latihan15/logika/IF110119023Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhammad Farhan R
+ * KELAS    : IF1
+ * NIM      : 10119023
+ * Deskripsi Program : latihan menggunakan operator logika
+ */
+public class IF110119023Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b)); //a AND b
+        System.out.println("a || b = " + (a || b)); //a OR b
+        System.out.println("!a = " + !a); //NOT a
+        System.out.println("!(a && b) = " + !(a && b)); //NOT(a AND b)
+    }
+    
+}
